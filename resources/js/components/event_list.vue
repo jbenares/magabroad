e79b0b59a5904281b2a04f6f984e736533cc1c2b
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2025-01-29 22:52:46 +0800

    Events UI

---
+++ resources/js/components/event_list.vue
@@ -0,0 +1,247 @@
+<template>
+	<navigation>
+		<div class="hero-wrap hero-wrap-2" >
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row no-gutters slider-text align-items-end justify-content-start">
+				<div class="col-md-12 text-center mb-5">
+					<p class="breadcrumbs mb-0"><span class="mr-3"><a href="/">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Events</span></p>
+					<h1 class="mb-3 bread">Upcoming Events</h1>
+				</div>
+				</div>
+			</div>
+		</div>
+
+		<section class="ftco-section">
+			<div class="container">
+				<!-- <div class="row">
+					<div class="col-md-12">
+						<div class="bg-gradient-to-r from-blue-400 to-blue-600 w-full rounded-2xl p-4 px-5 mb-5 ">
+							<h2 class="text-white font-semibold mb-0 leading">Dont miss this out! </h2>
+							<p class="text-white"></p>
+							<br>
+						</div>
+					</div>
+				</div> -->
+				<div class="row d-flex">
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_1.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading overflow-hidden text-ellipsis line-clamp-3 mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</h3>
+								<button class="see-more text-blue-500 bg-transparent border-none cursor-pointer mt-2">See more</button>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_7.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading">Even the all-powerful Pointing has no control about the blind texts</h3>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_5.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading">Even the all-powerful Pointing has no control about the blind texts</h3>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_3.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading">Even the all-powerful Pointing has no control about the blind texts</h3>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_4.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading">Even the all-powerful Pointing has no control about the blind texts</h3>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_1.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading overflow-hidden text-ellipsis line-clamp-3 mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</h3>
+								<button class="see-more text-blue-500 bg-transparent border-none cursor-pointer mt-2">See more</button>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 d-flex">
+						<a href="/event_view" class="blog-entry align-self-stretch shadow-md rounded-md">
+							<div class="block-20">
+								<div class="w-full h-64 overflow-hidden">
+								<img src="../../images/job_7.jpg" class="w-full h-full object-cover rounded-t-md" alt="Wide Image">
+								</div>
+							</div>
+							<div class="text p-3">
+								<div class="flex justify-start space-x-2 text-sm text-blue-600">
+								<span>August 28, 2019</span>
+								<span>|</span>
+								<span>Admin</span>
+								</div>
+								<h3 class="heading overflow-hidden text-ellipsis line-clamp-3 mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</h3>
+								<button class="see-more text-blue-500 bg-transparent border-none cursor-pointer mt-2">See more</button>
+
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="row mt-5">
+				<div class="col text-center">
+					<div class="block-27">
+					<ul>
+						<li><a href="#">&lt;</a></li>
+						<li class="active"><span>1</span></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li><a href="#">&gt;</a></li>
+					</ul>
+					</div>
+				</div>
+				</div>
+			</div>
+		</section>
+		
+	
+		<footer class="ftco-footer ftco-bg-dark ftco-section">
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col-md">
+						<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Skillhunt Jobboard</h2>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+							<!-- <li class="><a href="#"><span class="icon-twitter"></span></a></li>
+							<li class="><a href="#"><span class="icon-facebook"></span></a></li>
+							<li class="><a href="#"><span class="icon-instagram"></span></a></li> -->
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+					<h2 class="ftco-heading-2">Employers</h2>
+					<ul class="list-unstyled">
+						<li><a href="#" class="pb-1 d-block">Browse Candidates</a></li>
+						<li><a href="#" class="pb-1 d-block">Post a Job</a></li>
+						<li><a href="#" class="pb-1 d-block">Employer Listing</a></li>
+						<li><a href="#" class="pb-1 d-block">Resume Page</a></li>
+						<li><a href="#" class="pb-1 d-block">Dashboard</a></li>
+						<li><a href="#" class="pb-1 d-block">Job Packages</a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+					<h2 class="ftco-heading-2">Candidate</h2>
+					<ul class="list-unstyled">
+						<li><a href="#" class="pb-1 d-block">Browse Jobs</a></li>
+						<li><a href="#" class="pb-1 d-block">Submit Resume</a></li>
+						<li><a href="#" class="pb-1 d-block">Dashboard</a></li>
+						<li><a href="#" class="pb-1 d-block">Browse Categories</a></li>
+						<li><a href="#" class="pb-1 d-block">My Bookmarks</a></li>
+						<li><a href="#" class="pb-1 d-block">Candidate Listing</a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+					<h2 class="ftco-heading-2">Account</h2>
+					<ul class="list-unstyled">
+						<li><a href="#" class="pb-1 d-block">My Account</a></li>
+						<li><a href="#" class="pb-1 d-block">Sign In</a></li>
+						<li><a href="#" class="pb-1 d-block">Create Account</a></li>
+						<li><a href="#" class="pb-1 d-block">Checkout</a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Have a Questions?</h2>
+						<div class="block-23 mb-3">
+						<ul>
+							<li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+							<li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+							<li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+						</ul>
+						</div>
+					</div>
+				</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	</navigation>
+</template>
+
+<script setup>
+	import navigation from '@/layouts/navigation.vue';
+</script>
