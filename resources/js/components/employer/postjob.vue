56fcb7fc9336cbb819b472c455f24bd1c9d79383
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2025-04-06 03:07:31 +0800

    skill add

--- resources/js/components/employer/postjob.vue
+++ resources/js/components/employer/postjob.vue
@@ -15,19 +15,19 @@
 							<!-- <br> -->
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="jobtitle">Job Title</label>
+									<label class="font-weight-bold leading-none text-gray-500" for="jobtitle">Job Title</label>
 									<input type="text" id="jobtitle" class="form-control" placeholder="">
 								</div>
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="location">Location</label>
+									<label class="font-weight-bold leading-none text-gray-500" for="location">Location</label>
 									<input type="text" id="location" class="form-control" placeholder="">
 								</div>
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="workplace">Workplace Option</label>
+									<label class="font-weight-bold leading-none text-gray-500" for="workplace">Workplace Option</label>
 									<select id="workplace" class="form-control" >
 										<option value="">On-site</option>
 										<option value="">Remote</option>
@@ -37,7 +37,7 @@
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="workplace">Work Type</label>
+									<label class="font-weight-bold leading-none text-gray-500" for="workplace">Work Type</label>
 									<label for="fulltime" class="block !mb-0">
 										<input type="radio" id="fulltime" class="mr-2" name="worktype">Full-time
 									</label>
@@ -54,7 +54,7 @@
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="workplace">Pay Type</label>
+									<label class="font-weight-bold leading-none text-gray-500" for="workplace">Pay Type</label>
 									<label for="hourly" class="block !mb-0">
 										<input type="radio" id="hourly" class="mr-2" name="paytype">Hourly Rate
 									</label>
@@ -66,7 +66,7 @@
 									</label>
 								</div>
 							</div>
-							<label class="font-weight-bold" for="workplace">Pay Range</label>
+							<label class="font-weight-bold leading-none text-gray-500" for="workplace">Pay Range</label>
 							<div class="row form-group">
 								<div class="col-md-3 mb-3 mb-md-0 !pr-0">
 									<label class="mb-0" for="workplace">Currency</label>
@@ -85,145 +85,98 @@
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="jobdesc">Job Description</label>
-									<textarea id="jobdesc" class="form-control" placeholder=""></textarea>
+									<label class="font-weight-bold leading-none text-gray-500">Job Description</label>
+									<div class="toolbar flex gap-0 mb-1 border rounded-md">
+										<button type="button" class="py-1 px-2 border-r" @click="jobDescEditor.chain().focus().toggleBold().run()" :class="{ 'bg-gray-200': jobDescEditor.isActive('bold') }"><BoldIcon class="size-4"/></button>
+										<button type="button" class="py-1 px-2 border-r" @click="jobDescEditor.chain().focus().toggleItalic().run()" :class="{ 'bg-gray-200': jobDescEditor.isActive('italic') }"><ItalicIcon class="size-4"/></button>
+										<!-- <button type="button" class="py-1 px-2 border-r" @click="console.log('Bullet clicked');jobDescEditor.chain().focus().toggleBulletList().run()" :class="{ 'bg-gray-200': jobDescEditor.isActive('bulletList') }"><ListBulletIcon class="size-4"/></button>
+										<button type="button" class="py-1 px-2 border-r" @click="jobDescEditor.chain().focus().toggleOrderedList().run()" :class="{ 'bg-gray-200': jobDescEditor.isActive('orderedList') }"><NumberedListIcon class="size-4"/></button> -->
+									</div>
+									<div class="border rounded-md pt-2 pb-10 px-4 bg-white">
+										<editor-content :editor="jobDescEditor" class="prose max-w-none focus:outline-none focus:ring-0" />
+									</div>
 								</div>
 							</div>
 							<div class="row form-group">
 								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="jobsum">Job Summary</label>
-									<textarea id="jobsum" class="form-control" placeholder=""></textarea>
+									<label class="font-weight-bold leading-none text-gray-500">Job Summary</label>
+									<div class="toolbar flex gap-0 mb-1 border rounded-md">
+										<button type="button" class="py-1 px-2 border-r" @click="jobSummaryEditor.chain().focus().toggleBold().run()" :class="{ 'bg-gray-200': jobSummaryEditor.isActive('bold') }"><BoldIcon class="size-4"/></button>
+										<button type="button" class="py-1 px-2 border-r" @click="jobSummaryEditor.chain().focus().toggleItalic().run()" :class="{ 'bg-gray-200': jobSummaryEditor.isActive('italic') }"><ItalicIcon class="size-4"/></button>
+									</div>
+									<div class="border rounded-md pt-2 pb-10 px-4 bg-white">
+										<editor-content :editor="jobSummaryEditor" class="prose max-w-none focus:outline-none focus:ring-0" />
+									</div>
 								</div>
 							</div>
-							<!-- <div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="jobdesc">Responsibilities</label>
-									<div class="flex justify-start">
-										<input type="text" class="form-control">
-										<button class="btn !text-blue-500"> <PlusCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-								</div>
-							</div> -->
-							<!-- <div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-									<label class="font-weight-bold" for="jobdesc">Skills</label>
-									<div class="flex justify-start">
-										<input type="text" class="form-control">
-										<button class="btn !text-blue-500"> <PlusCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-									<div class="flex justify-start mt-1">
-										<input type="text" class="form-control">
-										<button class="btn !text-red-500"> <XCircleIcon class="size-6"/></button>
-									</div>
-								</div>
-							</div> -->
-							<div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-								<label class="font-weight-bold">Job Description</label>
-								<editor-content :editor="jobDescEditor" />
-								</div>
-							</div>
-
-							<div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-								<label class="font-weight-bold">Job Summary</label>
-								<editor-content :editor="jobSummaryEditor" />
-								</div>
-							</div>
-							<div class="box-container p-4 rounded-lg border border-gray-300 bg-white">
-								<label class="font-bold text-gray-700 mb-2 block">Responsibilities</label>
-
-								<!-- Input Box -->
+							<div class="mb-4">
+								<label class="font-bold text-gray-500 mb-2 block">Responsibilities</label>
 								<div class="flex items-center border border-gray-400 rounded-md px-3 py-2">
-								<input 
-									v-model="newResponsibility" 
-									@keyup.enter="addResponsibility" 
-									type="text" 
-									class="flex-grow outline-none text-gray-700" 
-									placeholder="Enter a responsibility"
-								/>
-								<button @click.prevent="addResponsibility()" class="text-green-500">
-									<PlusCircleIcon class="w-6 h-6" />
-								</button>
+									<input v-model="newResponsibility" @keyup.enter="addResponsibility" type="text" class="flex-grow outline-none text-gray-500" placeholder="Enter a responsibility" />
+									<button @click.prevent="addResponsibility()" class="text-blue-500">
+										<PlusCircleIcon class="w-6 h-6" />
+									</button>
 								</div>
-
-								<!-- Responsibilities List -->
-								<div v-if="responsibilities.length" class="mt-3">
-								<ul class="space-y-2">
-									<li 
-									v-for="(responsibility, index) in responsibilities" 
-									:key="index" 
-									class="responsibility-item flex justify-between items-center bg-green-100 px-3 py-2 rounded-lg"
-									>
-									<span class="text-gray-700 font-medium">{{ responsibility }}</span>
-									<XCircleIcon 
-										@click="removeResponsibility(index)" 
-										class="w-5 h-5 text-red-500 cursor-pointer hover:text-red-700"
-									/>
-									</li>
-								</ul>
+								<div v-if="responsibilities.length" class="mt-2">
+									<ul class="space-y-1">
+										<li v-for="(responsibility, index) in responsibilities" :key="index" class="responsibility-item flex justify-between items-center px-3 py-1">
+											<span class="w-5 font-bold text-gray-500">{{ index + 1 }}.</span>
+											<div class="flex justify-between items-center border-b w-full">
+												<span class="text-gray-500 font-medium ">{{ responsibility }}</span>
+												<XCircleIcon @click="removeResponsibility(index)" class="w-5 h-5 text-red-500 cursor-pointer hover:text-red-700"/>
+											</div>
+										</li>
+									</ul>
 								</div>
 							</div>
-							<div class="box-container p-4 rounded-lg border border-gray-300 bg-white">
-								<label class="font-bold text-gray-700 mb-2 block">Skills</label>
-
-								<!-- Input Box -->
+							<div class="mb-4">
+								<label class="font-bold text-gray-500 mb-2 block">Skills</label>
+								<div class="relative">
 								<div class="flex items-center border border-gray-400 rounded-md px-3 py-2">
-								<input 
+									<input 
 									v-model="newSkill" 
+									@input="filterSuggestions" 
 									@keyup.enter="addSkill" 
 									type="text" 
-									class="flex-grow outline-none text-gray-700" 
+									class="flex-grow outline-none text-gray-500" 
 									placeholder="Enter a skill"
-								/>
-								<button @click.prevent="addSkill()" class="text-blue-500">
+									/>
+									<button @click.prevent="addSkill()" class="text-blue-500">
 									<PlusCircleIcon class="w-6 h-6" />
-								</button>
-								<!-- Selected Skills -->
+									</button>
 								</div>
-									<div class="mt-3 flex flex-wrap gap-2">
-									<span 
-										v-for="(skill, index) in skills" 
-										:key="index" 
-										class="selected-skill bg-blue-500 text-white px-3 py-1 rounded-lg flex items-center"
-									>
-										{{ skill }}
-										<XCircleIcon @click="removeSkill(index)" class="w-4 h-4 ml-2 cursor-pointer hover:text-red-300" />
-									</span>
-								</div>
-								<!-- Suggested Skills -->
-								<div v-if="skillSuggestions.length" class="mt-3 p-2 bg-gray-100 rounded-md">
-								<span 
-									v-for="(suggestion, index) in skillSuggestions" 
+								
+								<!-- Dropdown suggestions -->
+								<div v-if="filteredSuggestions.length" class="absolute top-full left-0 w-full mt-1 bg-white border border-gray-400 rounded-md shadow-lg z-10">
+									<div 
+									v-for="(suggestion, index) in filteredSuggestions" 
 									:key="index" 
 									@click="addSkill(suggestion)" 
-									class="suggestion-chip cursor-pointer bg-gray-200 hover:bg-gray-300 text-gray-800 px-2 py-1 rounded-md m-1 inline-block"
+									class="cursor-pointer p-2 hover:bg-gray-200"
+									>
+									{{ suggestion }}
+									</div>
+								</div>
+								</div>
+
+								<!-- Selected skills -->
+								<div class="mt-3 flex flex-wrap gap-2">
+								<span 
+									v-for="(skill, index) in skills" 
+									:key="index"  
+									class="selected-skill bg-blue-500 text-white px-3 py-1 rounded-lg flex items-center"
 								>
-									+ {{ suggestion }}
+									{{ skill }}
+									<XCircleIcon @click="removeSkill(index)" class="w-4 h-4 ml-2 cursor-pointer hover:text-red-300" />
 								</span>
+								<div v-if="skillSuggestions.length" class="mt-3 p-2 bg-gray-100 rounded-md">
+									<span class="block w-full font-bold px-2 text-sm">Suggested Skill</span>
+									<span v-for="(suggestion, index) in skillSuggestions" :key="index" @click="addSkill(suggestion)"class="suggestion-chip cursor-pointer bg-gray-200 hover:bg-gray-300 text-gray-800 px-2 py-1 rounded-md m-1 inline-block" > 
+										+ {{ suggestion }} 
+									</span>
+								</div>
 								</div>
 							</div>
-							<br>
 							<hr>
 							<div class="row form-group">
 								<div class="col-md-12">
@@ -234,7 +187,6 @@
 											<input type="submit" value="Post Job" class="btn btn-primary  py-2 px-5">
 										</div>
 									</div>
-									
 								</div>
 							</div>
 						</form>
@@ -245,14 +197,12 @@
 						<h3 class="h5 text-black mb-3">Contact Info</h3>
 						<p class="mb-0 font-weight-bold">Address</p>
 						<p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
-
 						<p class="mb-0 font-weight-bold">Phone</p>
 						<p class="mb-4"><a href="#">+1 232 3235 324</a></p>
 
 						<p class="mb-0 font-weight-bold">Email Address</p>
 						<p class="mb-0"><a href="#"><span class="" data-cfemail="">[email&#160;protected]</span></a></p>
 					</div>
-					
 					<div class="p-4 mb-3 bg-white">
 						<h3 class="h5 text-black mb-3">More Info</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad iure porro mollitia architecto hic consequuntur. Distinctio nisi perferendis dolore, ipsa consectetur</p>
@@ -360,30 +310,68 @@
 </template>
 <script setup>
 	import navigation from '@/layouts/navigation_employer.vue';
-	import { PlusCircleIcon, XCircleIcon } from "@heroicons/vue/24/solid";
+	import { PlusCircleIcon, XCircleIcon, ListBulletIcon, NumberedListIcon, ItalicIcon, BoldIcon} from "@heroicons/vue/24/solid";
 	import { ref, onMounted, onBeforeUnmount } from "vue";
 	import { Editor, EditorContent } from "@tiptap/vue-3";
-	import StarterKit from "@tiptap/starter-kit";
+	import StarterKit from '@tiptap/starter-kit'
+	import Blockquote from '@tiptap/extension-blockquote'
+	import BulletList from '@tiptap/extension-bullet-list'
+	import OrderedList from '@tiptap/extension-ordered-list'
+	import ListItem from '@tiptap/extension-list-item'
 
-	const jobDescEditor = ref(null);
-	const jobSummaryEditor = ref(null);
 
-	onMounted(() => {
-	jobDescEditor.value = new Editor({
-		extensions: [StarterKit],
-		content: "<p>Type job description here...</p>",
+	const jobDescEditor = new Editor({
+	extensions: [
+		StarterKit,
+		Blockquote,
+		BulletList,
+		OrderedList,
+		ListItem,
+	],
+	content: '<p>Type job description here...</p>',
 	});
 
-	jobSummaryEditor.value = new Editor({
-		extensions: [StarterKit],
-		content: "<p>Type job summary here...</p>",
+	const jobSummaryEditor = new Editor({
+	extensions: [
+		StarterKit,
+		Blockquote,
+		BulletList,
+		OrderedList,
+		ListItem,
+	],
+	content: '<p>Type job description here...</p>',
 	});
-	});
+	// const jobDescEditor = ref(null);
+	// const jobSummaryEditor = ref(null);
 
-	onBeforeUnmount(() => {
-	jobDescEditor.value.destroy();
-	jobSummaryEditor.value.destroy();
-	});
+	// onMounted(() => {
+	// 	jobDescEditor.value = new Editor({
+	// 		extensions: [
+	// 			StarterKit,
+	// 			Blockquote,
+	// 			BulletList,
+	// 			OrderedList,
+	// 			ListItem,
+	// 		],
+	// 		content: "<p>Type job description here...</p>",
+	// 	});
+
+	// 	jobSummaryEditor.value = new Editor({
+	// 		extensions: [
+	// 			StarterKit,
+	// 			Blockquote,
+	// 			BulletList,
+	// 			OrderedList,
+	// 			ListItem,
+	// 		],
+	// 		content: "<p>Type job summary here...</p>",
+	// 	});
+	// });
+
+	// onBeforeUnmount(() => {
+	// jobDescEditor.value.destroy();
+	// jobSummaryEditor.value.destroy();
+	// });
 
 	// Reactive variables
 	const newSkill = ref("");
@@ -400,6 +388,7 @@
 	"Strategic Leadership",
 	"Computer Security"
 	]);
+	const filteredSuggestions = ref([]);
 
 	// Add skill function
 	const addSkill = (skill) => {
@@ -407,6 +396,7 @@
 	if (skill && !skills.value.includes(skill)) {
 		skills.value.push(skill);
 		newSkill.value = "";
+		filteredSuggestions.value = [];
 	}
 	};
 
@@ -414,6 +404,25 @@
 	const removeSkill = (index) => {
 	skills.value.splice(index, 1);
 	};
+
+	// Filter suggestions based on input
+	const filterSuggestions = () => {
+	if (!newSkill.value) {
+		filteredSuggestions.value = [];
+		return;
+	}
+	filteredSuggestions.value = skillSuggestions.value.filter(suggestion =>
+		suggestion.toLowerCase().includes(newSkill.value.toLowerCase())
+	);
+	};
+
+	onMounted(() => {
+	// Code to run on mount if needed
+	});
+
+	onBeforeUnmount(() => {
+	// Cleanup if necessary before the component is destroyed
+	});
 
 	// Reactive variables
 	const newResponsibility = ref("");
